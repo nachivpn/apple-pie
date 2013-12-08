c71bcae519e1f510a9d2a624902e9fa16ee135a3
nachivpn <nachivpn@gmail.com>
2013-12-08 21:35:02 +0530

    error fix & reduced comparisons

--- array/shiftodd.c
+++ array/shiftodd.c
@@ -7,6 +7,8 @@ Given an array of even and odd numbers(unsorted)
 - Do not use more than one loop for computation
 - Do no use more than two variables inside the loop
 
+Error fix & optimization credits: Gautham (https://github.com/Gautham)
+
 Example:
 
 I/P: 1 2 3 4 5 6 7 8 9 10
@@ -15,11 +17,7 @@ O/P: 1 3 5 7 9 0 0 0 0 0
 */
 
 #include<stdio.h>
-#define MAX 10
-int iseven(int n)
-{
-	return n%2;
-}
+#define MAX 5
 
 int main()
 {
@@ -31,35 +29,21 @@ int main()
 		scanf("%d",&array[i]);
 
 	/* The two variables: head -> earliest 0 in the array	tail -> last 0 in the array*/
-	int head=-1,tail=-1;
+	int head=0;
 
 	/*	Computation	*/
 
 	for(i=0;i<MAX;i++)
 	{
-		if(iseven(array[i])==0)
+		if(array[i]%2)
 		{
-			/* Replace all even number by 0 */
-			array[i]=0;	
-			if(head == -1)
-				head = tail = i;
-			else 
-				tail = i; 
+			array[head]=array[i];
+			if( head != i)
+				array[i]=0;
+			head++; 
 		}
 		else
-		{
-			if(head!=-1)
-			{
-				/* Push odd number to the left (Exchange with the earliest 0) */
-				array[head] = array[head]+array[i];	
-				array[i]= array[head] - array[i];
-				array[head] = array[head] - array[i];
-				if(head<tail)
-					head++;
-				else if(head==tail)
-					head=tail=i;
-			}
-		}
+			array[i]=0;
 	}
 
 	/*	Output	*/
